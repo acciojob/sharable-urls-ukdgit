511a89740241e0ea5a469b051139c833cd55baa9
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-06 20:04:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// your code here
+function generateURL(){
+    const name = document.getElementById('name').value;
+    const year = document.getElementById('year').value;
+    let url = 'https://localhost:8080/';
+    if(name.length + year.length > 0) url += '?';
+    if(name!='') url += 'name=' + name;
+    if(year!=''){
+      if(name!='') url += '&'
+      url += 'year=' + year;
+    }
+    document.getElementById('url').innerHTML = url;
+  }
+  document.getElementById('button').addEventListener('click', generateURL);
